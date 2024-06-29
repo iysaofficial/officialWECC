d428d18187ffcb7ce14a602fb1feb122831b81c2
Nay <Nay@gmail.com>
2024-06-29 13:10:39 +0700

    open

--- src/components/compfaq.jsx
+++ src/components/compfaq.jsx
@@ -16,7 +16,7 @@ export default function Faq() {
 
   return (
     <>
-    {/* PAGE HEADER START */}
+    
       <div className="page-header text-center">
         <h1>FAQ</h1>
         <a href="/">Home</a>
