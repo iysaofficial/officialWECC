44c9f7c9df8bb37a77caf55851e7222cbcf293ef
unknown <youngscientist.iysa@gmail.com>
2025-05-27 10:03:39 +0700

    udinus logo 2 -k

--- src/components/organized.jsx
+++ src/components/organized.jsx
@@ -1,30 +1,39 @@
 function Organized() {
-  
-    return (
-      <>
+  return (
+    <>
       <section className="organized-section">
         <div className="site-section bg-left-half mb-5">
-            <div className="container owl-2-style">
-                <h1 className=" py-5 ">Organized By :</h1>
-                <div className="owl-carousel owl-2">
-                    <div className="content-image">
-                        <img src="./assets/logo/LogoIYSA.WebP" alt="Image" className="img-fluid" />
-                        <h3><a>Indonesian Young Scientist Association</a></h3>
-                    </div>
-                    <div className="content-image">
-                        <img src="./assets/logo/LogoIYSA.WebP" alt="Image" className="img-fluid" />
-                        <h3><a>Indonesian Young Scientist Association</a></h3>
-                    </div>
-                    <div className="content-image">
-                        <img src="./assets/logo/logoudinus.png" alt="Image" className="img-fluid" />
-                        <h3><a>Universitas Dian Nuswantoro</a></h3>
-                    </div>
+          <div className="container text-center py-5">
+            <h1 className="mb-5 fw-bold">Organized By :</h1>
+            <div className="row justify-content-center align-items-center">
+              <div className="col-12 col-md-4 mb-4 mb-md-0">
+                <div className="content-image">
+                  <img
+                    src="./assets/logo/LogoIYSA.WebP"
+                    alt="Indonesian Young Scientist Association"
+                    className="img-fluid"
+                    style={{ maxHeight: "120px", objectFit: "contain" }}
+                  />
+                  <h5 className="mt-3">Indonesian Young Scientist Association</h5>
                 </div>
+              </div>
+              <div className="col-12 col-md-4 mb-4 mb-md-0">
+                <div className="content-image">
+                  <img
+                    src="./assets/logo/logoudinus.png"
+                    alt="Universitas Dian Nuswantoro"
+                    className="img-fluid"
+                    style={{ maxHeight: "120px", objectFit: "contain" }}
+                  />
+                  <h5 className="mt-3">Universitas Dian Nuswantoro</h5>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
       </section>
-      </>
-    )
-  }
-  
-  export default Organized
+    </>
+  );
+}
+
+export default Organized;
