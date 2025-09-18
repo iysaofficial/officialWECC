3817532dce2e62fe274203a5ad54ccb35bf58309
iysaofficial <youngscientist.iysa@gmail.com>
2025-09-18 10:02:54 +0700

    news 2025

--- src/pages/news/listnews.jsx
+++ src/pages/news/listnews.jsx
@@ -13,6 +13,37 @@ function ListNews() {
       {/* PAGE HEADER END */}
       <section className="news-section">
         <div className="text-center">
+          <h1 className="fw-bold">News 2025</h1>
+          <div className="wrapper row">
+            {/* NEWS 1 START */}
+            <div className="col-lg-3 card">
+              <img
+                className="img-fluid mb-2"
+                src="./assets/news/20251.jpg"
+                alt=""
+              />
+              <h5>
+                GloCoLiS, ISPC, and WECC 2025 Successfully Held at Dian Nuswantoro University, Semarang
+              </h5>
+              <p>
+                {" "}
+                Jabaran.id – The Indonesian Young Scientist Association (IYSA) has once again successfully held three prestigious international competitions, namely GloCoLiS (Global Competition for Life Sciences), International Social Project Competition (ISPC), and World Essay Competition Championship (WECC). This year marks a special milestone with
+                the 5th anniversary of GloCoLiS, as well as the 2nd anniversary of ISPC and WECC.Translated with DeepL.com (free version)
+              </p>
+              <a
+                href="https://jabaran.id/glocolis-ispc-dan-wecc-2025-sukses-diselenggarakan-di-universitas-dian-nuswantoro-semarang/"
+                target="_blank"
+                className="btn btn-action"
+              >
+                See More
+              </a>
+            </div>
+            {/* NEWS 1 END */}
+          </div>
+        </div>
+      </section>
+      <section className="news-section">
+        <div className="text-center">
           <h1 className="fw-bold">News 2024</h1>
           <div className="wrapper row">
             {/* NEWS 1 START */}
