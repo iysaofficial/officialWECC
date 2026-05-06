617a21b01b65c25d592baccfbaffb54b8b62e474
iysaofficial <youngscientist.iysa@gmail.com>
2026-05-06 10:18:43 +0700

    UPDATE CLOSE

--- src/components/mainhero.jsx
+++ src/components/mainhero.jsx
@@ -8,11 +8,12 @@ function MainHero() {
           <div className="container px-5 py-6 py-xxl-10 ">
             <div className="row justify-content-md-center">
               <div className="content col-12 col-lg-9 col-xl-7 col-xxl-6 text-center">
-                <h1 className="display-3 fw-bold mb-3">WECC</h1>
-                <p className="lead">Register for the third World Economics Challenge and Competition (WECC) organized by the Indonesian Young Scientist Association (IYSA)</p>  
-                <p className="lead mb-5">Join us and be part of the next generation that is prepared for a bright future!</p>
+                {/* <h1 className="display-3 fw-bold mb-3">WECC </h1> */}
+                <h1 className="display-3 fw-bold mb-3">COMING SOON WECC 2027</h1>
+                <p className="lead">World Economics Challenge and Competition (WECC) organized by the Indonesian Young Scientist Association (IYSA)</p>  
+                {/* <p className="lead mb-5">Join us and be part of the next generation that is prepared for a bright future!</p> */}
                 <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                  <a href="#" type="button" className="btn btn-action btn-lg px-4 gap-3">Close Registration</a>
+                  <a href="#" type="button" className="btn btn-action btn-lg px-4 gap-3">Closed Registration !</a>
                   {/* <a href="/homeregist" type="button" className="btn btn-action btn-lg px-4 gap-3">Regist here!!</a> */}
                   {/* <a href="https://drive.google.com/drive/folders/1kGCLARfqUwZeXH4hUuddl2894oaDg4gL?usp=sharinga" target="_blank" rel="noopener noreferrer" type="button" className="btn btn-outline-light btn-lg px-4">Guide book</a> */}
                 </div>
